a3e33b8921e6fd4378f490892c82359e018a28b7
jambis <imjaamees@gmail.com>
2019-09-05 18:01:59 -0500

    Completed step 5 with some stretch goals

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -15,11 +15,11 @@
            create a new component and add it to the DOM as a child of .cards
 */
 const cardsDiv = document.querySelector(".cards");
+const baseURL = "https://api.github.com/users/tetondan";
 
 axios
-  .get("https://api.github.com/users/jambis")
+  .get(baseURL)
   .then(res => {
-    console.log(res);
     const newCard = createCard(res.data);
     cardsDiv.appendChild(newCard);
   })
@@ -37,8 +37,27 @@ axios
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
+axios
+  .get(`${baseURL}/followers`)
+  .then(res => {
+    const followersArray = [...res.data];
+    followersArray.forEach(follower => {
+      axios
+        .get(follower.url)
+        .then(res => {
+          const newFollower = createCard(res.data);
+          cardsDiv.appendChild(newFollower);
+        })
+        .catch(err => {
+          console.log("The follower data was not returned", err);
+        });
+    });
+    // const newCard = createCard(res.data);
+    // cardsDiv.appendChild(newCard);
+  })
+  .catch(err => {
+    console.log("The data was not returned", err);
+  });
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
 
@@ -80,12 +99,12 @@ function createCard(data) {
   cardImg.src = data.avatar_url;
   nameH3.textContent = data.name;
   userNameP.textContent = data.login;
-  locationP.textContent = data.location;
+  locationP.textContent = `Location: ${data.location}`;
   profileP.textContent = "Profile: ";
   profileA.textContent = data.html_url;
   profileA.href = data.html_url;
-  followersP.textContent = data.followers;
-  followingP.textContent = data.following;
+  followersP.textContent = `Followers: ${data.followers}`;
+  followingP.textContent = `Following: ${data.following}`;
   bioP.textContent = data.bio;
 
   cardDiv.append(cardImg, cardInfoDiv);
@@ -101,6 +120,20 @@ function createCard(data) {
     bioP
   );
 
+  axios
+    .get(`https://api.github.com/users/${data.login}/repos`)
+    .then(res => {
+      let repoArray = [...res.data];
+      let sortedRepoArray = repoArray.sort((a, b) => {
+        return Date.parse(b.updated_at) - Date.parse(a.updated_at);
+      });
+      sortedRepoArray.slice(0, 5).forEach(repo => {
+        const newRepo = createRepoCard(repo);
+        cardDiv.appendChild(newRepo);
+      });
+    })
+    .catch(err => console.log(err));
+
   return cardDiv;
 }
 
@@ -111,3 +144,34 @@ function createCard(data) {
   luishrd
   bigknell
 */
+
+//Calendar stuff
+const calendarDiv = document.createElement("div");
+const containerDiv = document.querySelector(".container");
+
+calendarDiv.classList.add("calendar");
+containerDiv.insertBefore(calendarDiv, cardsDiv);
+
+const calendar = new GitHubCalendar(".calendar", "tetondan", {
+  responsive: true
+});
+
+//Expanding card stuff
+function createRepoCard(data) {
+  const repoDiv = document.createElement("div");
+  const repoName = document.createElement("p");
+  const repoLink = document.createElement("a");
+  const repoUpdated = document.createElement("p");
+
+  //cardDiv.classList.add("card");
+
+  repoName.text = "Repository Name: ";
+  repoLink.href = data.html_url;
+  repoLink.textContent = data.name;
+  repoUpdated.textContent = `Last Updated: ${data.updated_at}`;
+
+  repoName.append(repoLink);
+  repoDiv.append(repoName, repoUpdated);
+
+  return repoDiv;
+}
